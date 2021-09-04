c566d00c4b00f0dbd3d0e083767092b48cd97ffe
fznaor <fznaor@gmail.com>
2021-09-04 13:27:04 +0200

    Updated db credentials

--- scripts/php/db/dbconfig.php
+++ scripts/php/db/dbconfig.php
@@ -2,8 +2,8 @@
 
 final class DBConfig
 {
-    const HOST = 'db';
-    const DB_NAME = 'ferit';
-    const USERNAME = 'user';
-    const PASS = 'test';
+    const HOST = 'eu-cdbr-west-01.cleardb.com';
+    const DB_NAME = 'heroku_443128146797a2c';
+    const USERNAME = 'bb961d1cbf1048';
+    const PASS = '31492181';
 }
